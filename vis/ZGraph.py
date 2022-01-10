04488aed1f533e63eb71ea0c1c25b6501ba98262
Karl Zipser <karlzipser@berkeley.edu>
2022-01-10 08:07:11 -0800

    gacp

--- vis/ZGraph.py
+++ vis/ZGraph.py
@@ -1,5 +1,5 @@
 
-from utilz.vis.cv2_ import *
+from utilz.vis import *
 
 
 
@@ -102,11 +102,7 @@ class ZGraph:
         _.ymin = None
         _.ymax = None
         _.aspect_ratio = 1.0
-<<<<<<< HEAD
         _.pixels_list = []
-=======
-        _.pixels = []
->>>>>>> a2f601e9987f0572eba136c0c3a54ca2b84641e2
 
     def add(_,xys,color=(255,255,255),line_endpoint_indicies=[]):
         _.xys_color_list.append((na(xys),color,line_endpoint_indicies))
@@ -115,7 +111,7 @@ class ZGraph:
         xmins = []
         xmaxes = []
         ymins = []
-        for xys,color in _.xys_color_list:
+        for xys,color,line_endpoint_indicies in _.xys_color_list:
             xmins.append(xys[:,0].min())
             xmaxes.append(xys[:,0].max())
             ymins.append(xys[:,1].min())
@@ -125,10 +121,7 @@ class ZGraph:
             _.xmax = max(xmaxes)
         if is_None(_.ymin):
             _.ymin = min(ymins)
-<<<<<<< HEAD
 
-=======
->>>>>>> a2f601e9987f0572eba136c0c3a54ca2b84641e2
 
     def graph(
         _,
@@ -147,7 +140,6 @@ class ZGraph:
             _._find_min_max()
 
         _.plot_called = True
-<<<<<<< HEAD
 
         for xys,color,line_endpoint_indicies in _.xys_color_list:
             pixels = pts2img(_.img,xys,_.xmin,_.xmax,_.ymin,_.aspect_ratio,color)
@@ -175,49 +167,6 @@ class ZGraph:
                         (x0,shape(_.img)[0]-1-y0),(x1,shape(_.img)[0]-1-y1),
                         color=c,
                         thickness=thickness,
-=======
-        for xys,color in _.xys_color_list:
-            pixels = pts2img(_.img,xys,_.xmin,_.xmax,_.ymin,_.aspect_ratio,color)
-            if not len(_.pixels):
-                _.pixels = pixels
-            else:
-                _.pixels = np.concatenate((_.pixels,pixels))
-
-        if False:
-            pixels = _.pixels
-            line_endpoint_indicies = rndint(1000,size=(30,2))#((1,2),(4,9),(100,200))
-            valid_enpoint_indicies = []
-            p = pixels[:,2]
-            for a in line_endpoint_indicies:
-                si = np.where(p==a[0])[0]
-                ei = np.where(p==a[1])[0]
-                if len(si) > 0 and len(ei) > 0:
-                    valid_enpoint_indicies.append((si[0],ei[0]))
-            for j in rlen(valid_enpoint_indicies):
-                a,b = line_endpoint_indicies[j]
-                x0,y0 = pixels[a][:2]
-                x1,y1 = pixels[b][:2]
-                cg(x0,y0,x1,y1)
-                #p = pixels[i][2:]
-                #c = na([p[0],p[1],p[2]])
-                #c = c.astype(np.uint8)
-                #print(type(c[0]),type(c[1]))
-                print(pixels[a])
-                if len(pixels[a]) < 6:
-                    c = [255,0,0]
-                else:
-                    c = [0,255,0]
-                    c=[int(pixels[a][3]),int(pixels[a][4]),int(pixels[a][5])]
-                    #c = [0,255,0]
-                    cm(c)
-                #cm(c,r=1)
-                cv2.line(
-                    _.img,
-                    (x0,shape(_.img)[0]-1-y0),(x1,shape(_.img)[0]-1-y1),
-                    #(x0,y0),(x1,y1),
-                    color=c,
-                    thickness=3,
->>>>>>> a2f601e9987f0572eba136c0c3a54ca2b84641e2
                 )
     def show(_,scale=1.0):
         scale = float(scale)
@@ -283,56 +232,52 @@ def plot_xys(
 
 
 
-def _test_ZGraph():
-    xys=rndn(1000,2)
-    z=ZGraph(400,200,title='zgraph1')
-<<<<<<< HEAD
-    z.add(
-        xys=2*xys*na([-1,1])+na([8,-7]),
-        color=rndint(255,size=(len(xys),3)),
-        line_endpoint_indicies=rndint(400,size=(30,2)),
-    )
-    z.add(
-        xys=1*xys*na([-1,1])+na([12,3]),
-        color=zeros((len(xys),3),np.uint8)+(255,127,31),
-        line_endpoint_indicies=rndint(400,size=(30,2)),
-    )
-    z.add(xys+na([-5,4]),(50,255,255),line_endpoint_indicies=rndint(200,size=(30,2)))# rndint(255,size=(len(xys),3)))# 
-    z.graph(-9,9,-9,thickness=2)
-=======
-    z.add(3*xys*na([-1,1])+na([8,-6]),rndint(255,size=(len(xys),3)))#(255,128,0))#
-    z.add(xys+na([-5,4]), rndint(255,size=(len(xys),3)))#,(50,255,255)) 
-    z.graph(-9,9,-9)
->>>>>>> a2f601e9987f0572eba136c0c3a54ca2b84641e2
-    z.show(1.)
-    z.report()
-
-    if False:
-        z2=ZGraph(400,400,title='zgraph2')
-        z2.add(xys+na([-3,0]),rndint(255,size=(len(xys),3)))
-        z2.graph(-9,9,-9)
-        z2.report()
-        z2.show()
-
-    if False:
-        plot_xys(xys,img=z.img,width=300,height=300,xmin=-5,xmax=5,ymin=-5,aspect_ratio=1,title='plot_xys')
-        plot_xys(xys*0.1+na([0.4,1.3]),img=z.img,color=(30,127,0),width=300,height=300,xmin=-5,xmax=5,ymin=-5,aspect_ratio=1,title='plot_xys')
-
-
-    if False:
-        img = get_blank_rgb(100,100)
-        xys = rndn(1000,2)
-        pixels=pts2img(img,xys,-3,3,-3,1,color=rndint(256,size=(len(xys),3)))
-        mi(img)
-
-
-
-    raw_enter()
 
 
 
 if __name__ == '__main__':
     CA()
-    _test_ZGraph()
+
+    xys=rndn(1000,2)
+
+    if True:
+        z0=ZGraph(100,100,title='ZGraph z0')
+        z0.add(xys+na([-3,0]),rndint(255,size=(len(xys),3)))
+        z0.graph()
+        z0.report()
+        z0.show()
+
+    if True:
+        z1=ZGraph(400,200,title='ZGraph z1')
+        z1.add(
+            xys=2*xys*na([-1,1])+na([8,-7]),
+            color=rndint(255,size=(len(xys),3)),
+            line_endpoint_indicies=rndint(400,size=(30,2)),
+        )
+        z1.add(
+            xys=1*xys*na([-1,1])+na([12,3]),
+            color=zeros((len(xys),3),np.uint8)+(255,127,31),
+            line_endpoint_indicies=rndint(400,size=(30,2)),
+        )
+        z1.add(xys+na([-5,4]),(50,255,255),line_endpoint_indicies=rndint(200,size=(30,2)))# rndint(255,size=(len(xys),3)))# 
+        z1.graph(-9,9,-9,thickness=2)
+        z1.show(1.)
+        z1.report()
+
+
+
+    if True:
+        plot_xys(xys*0.3+na([0.4,1.3]),img=z1.img,color=(30,127,0),width=300,height=300,xmin=-5,xmax=5,ymin=-5,aspect_ratio=1,title='using plot_xys')
+
+
+    if True:
+        img = get_blank_rgb(100,100)
+        xys = rndn(1000,2)
+        pixels=pts2img(img,xys,-3,3,-3,1,color=rndint(256,size=(len(xys),3)))
+        mci(img,title='using pts2img')
+
+
+    raw_enter()
+
 
 
